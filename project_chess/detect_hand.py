70c6b5f4a22f3446441f723458bc4000604b83c8
wu <example@qq.com>
2024-12-09 12:15:55 +0800

    测试五张图都可以，改变提取多轮廓后的检测策略，采用大轮廓作为提取，避免再次缩小边框导致圆形出现空缺

--- project_chess/detect_hand.py
+++ project_chess/detect_hand.py
@@ -3,7 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # 1. 读取图像
-image_path = 'hand4.jpg'  # 替换为你的图像路径
+image_path = 'hand.jpg'  # 替换为你的图像路径
 
 
 def resize_image_with_padding(image, target_size):
@@ -74,7 +74,6 @@ def resize_and_pad_binary_image(binary_image, target_size):
 
     return new_img
 
-
 def find_chessboard(image_path):
     img = cv2.imread(image_path)
 
@@ -130,7 +129,12 @@ def get_grid_info(gray_chessboard):
     # 绘制小轮廓，大轮廓剔除
     for i, cnt in enumerate(contours):
         # 检查当前轮廓的层级，如果父轮廓的索引是 -1，说明它是第0层级
-        if hierarchy[0][i][3] == 2  or hierarchy[0][i][3] == 0:  # 1rd dimension indicates parent index
+        level = 0
+        parent_index = hierarchy[0][i][3]  # 父轮廓索引
+        while parent_index != -1:
+            level += 1
+            parent_index = hierarchy[0][parent_index][3]  # 父轮廓索引
+        if level == 1:  # 1rd dimension indicates parent index
             x, y, w, h = cv2.boundingRect(cnt)
             if w>200 or h>200 or w<25 or h<25:
                 continue
@@ -178,6 +182,7 @@ def arange_grid(point_array,threshold_y = 20,threshold_x = 20):
 def get_chessboard_states(lines,gray_chessboard,shrink_factor=7):
     h,w = len(lines[0]),len(lines)
     chessboard_states=np.zeros((h,w))
+    white_ratio_board = np.zeros((h,w))
     # count = 0
     for i,line in enumerate(lines):
         for j,point in enumerate(line):
@@ -187,8 +192,7 @@ def get_chessboard_states(lines,gray_chessboard,shrink_factor=7):
                 shrink_factor = 3
             else:
                 shrink_factor = 7
-            # region = dilated[y:y+h,x:x+w]
-            # shrink_factor = 7  # 定义收窄的像素值
+            
             x_shrinked =  x + shrink_factor  # 边界不超出原图像
             y_shrinked = y + shrink_factor
             w_shrinked = max(0, w - 2 * shrink_factor)  # 收窄宽度
@@ -196,22 +200,29 @@ def get_chessboard_states(lines,gray_chessboard,shrink_factor=7):
 
             # 确保收窄后的边界不会超出图像范围
             region = gray_chessboard[y_shrinked:y_shrinked + h_shrinked, x_shrinked:x_shrinked + w_shrinked]
+            # 腐蚀
+            
             total_pixels = region.size  # 总像素数
             white_pixels = np.sum(region == 255)  # 白色像素数
 
             # 计算白色像素占比
             white_ratio = white_pixels / total_pixels if total_pixels > 0 else 0
-            if white_ratio > 0.05:
+            white_ratio_board[j,i] = white_ratio
+            if white_ratio > 0.1:
                 #提取轮廓
                 contours, hierarchy = cv2.findContours(region, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
                 if len(contours) > 1:
-                    # print(f"区域 ({j}, {i}) 包含多个轮廓")
+                    logest_contour = max(contours, key=cv2.contourArea)
+                    perimeter = cv2.arcLength(logest_contour, True)
+                    area = cv2.contourArea(logest_contour)
+                    if perimeter > 0 and area > 0 and perimeter / (2 * np.pi * np.sqrt(area / np.pi)) < 1.3:
+                    
+                        chessboard_states[j,i]=1
+                    else:
+                        chessboard_states[j,i]=2
+                    
                     #用shrink_factor缩小轮廓
-                    notion = np.zeros_like(region)
-                    notion[shrink_factor:h_shrinked-shrink_factor,shrink_factor:w_shrinked-shrink_factor] = region[shrink_factor:h_shrinked-shrink_factor,shrink_factor:w_shrinked-shrink_factor]
-                    region = notion
-                    contours, hierarchy = cv2.findContours(region, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-                    # print(f"缩小后区域 ({j}, {i}) 包含 {len(contours)} 个轮廓")
+                    
                 #判断是否为圆形
                 if len(contours) == 1:
                     #计算轮廓的面积
@@ -224,126 +235,120 @@ def get_chessboard_states(lines,gray_chessboard,shrink_factor=7):
                         chessboard_states[j,i]=1
                     else:
                         chessboard_states[j,i]=2
-    return chessboard_states
-if __name__ == '__main__':
-    chessboard,gray_chessboard = find_chessboard(image_path)
-    plt.imshow(chessboard)
-    plt.show()
-    plt.imshow(gray_chessboard,cmap='gray')
-    plt.show()
+    return chessboard_states,white_ratio_board
 
-    point_array,coordinates_dict,resize_chessboard = get_grid_info(gray_chessboard)
-    plt.imshow(resize_chessboard)
-    plt.show()
-    lines = arange_grid(point_array)
-    print(lines)
-    chessboard_states = get_chessboard_states(lines,gray_chessboard)
-    print(chessboard_states)
+chessboard,gray_chessboard = find_chessboard(image_path)
+plt.imshow(chessboard)
+plt.show()
+plt.imshow(gray_chessboard,cmap='gray')
+plt.show()
 
-    # 2. 轮廓检测
-    # contours, hierarchy = cv2.findContours(gray_chessboard,cv2.RETR_TREE , cv2.CHAIN_APPROX_SIMPLE)
-# for i, cnt in enumerate(contours):
-#     # 绘制轮廓
-#     # 获取轮廓的边界框
-#     x, y, w, h = cv2.boundingRect(cnt)
+point_array,coordinates_dict,resize_chessboard = get_grid_info(gray_chessboard)
+contours, hierarchy = cv2.findContours(gray_chessboard,cv2.RETR_TREE , cv2.CHAIN_APPROX_SIMPLE)
+def show_hierarchy(contours,hierarchy):
+    for i, cnt in enumerate(contours):
+        # 绘制轮廓
+        # 获取轮廓的边界框
+        x, y, w, h = cv2.boundingRect(cnt)
 
-#     # 根据 hierachy 获取当前轮廓的层级
-#     level = 0
-#     parent_index = hierarchy[0][i][3]  # 父轮廓索引
-#     cv2.putText(resize_chessboard, str(parent_index), (x, y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255,), 1)
+        # 根据 hierachy 获取当前轮廓的层级
+        level = 0
+        parent_index = hierarchy[0][i][3]  # 父轮廓索引
+        
 
-#     if parent_index==2:
-#         cv2.drawContours(resize_chessboard, contours, i, (255,), 1)  # 绘制当前轮廓，白色
-
-#     while parent_index != -1:  # -1 表示没有父轮廓
-#         level += 1
-#         parent_index = hierarchy[0][parent_index][3]  # 跳到父轮廓
-# cv2.drawContours(resize_chessboard, contours, -1, (0, 255, 0), 3)  # 绘制棋盘轮廓
-
+        while parent_index != -1:  # -1 表示没有父轮廓
+            level += 1
+            parent_index = hierarchy[0][parent_index][3]  # 跳到父轮廓
+        cv2.putText(resize_chessboard, str(level), (x, y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255,), 1)
+    # cv2.drawContours(resize_chessboard, contours, -1, (0, 255, 0), 3)  # 绘制棋盘轮廓
+plt.imshow(resize_chessboard)
+plt.show()
+lines = arange_grid(point_array)
+# print(lines)
+chessboard_states,white_ratio_board = get_chessboard_states(lines,gray_chessboard)
+print(chessboard_states)
+# print(white_ratio_board)
 # 3. 显示结果
-# chessboard_copy = resize_image_with_padding(chessboard, 640)
-# for line in lines:
-#     for point in line:
-#         cv2.circle(chessboard_copy, point, 5, (255, 0, 0), -1)  # 绘制蓝色圆点
-#         x,y = point
-#         w,h = coordinates_dict[point]
-#         cv2.rectangle(chessboard_copy, (x, y), (x + w, y + h), (0, 255, 0), 10)  # 绘制矩形框
-#         plt.imshow(chessboard_copy)
-#         plt.show()
+chessboard_copy = resize_image_with_padding(chessboard, 640)
+def show_lines(lines,chessboard_copy):
+    for line in lines:
+        for point in line:
+            cv2.circle(chessboard_copy, point, 5, (255, 0, 0), -1)  # 绘制蓝色圆点
+            x,y = point
+            w,h = coordinates_dict[point]
+            cv2.rectangle(chessboard_copy, (x, y), (x + w, y + h), (0, 255, 0), 10)  # 绘制矩形框
+            plt.imshow(chessboard_copy)
+            plt.show()
 
-# plt.imshow(chessboard_copy)
-# plt.show()
-# #识别xx和圆形
-# plt.imshow(gray_chessboard,cmap='gray')
-# plt.show()
-# chessboard_copy = resize_image_with_padding(chessboard, 640)
-# print(len(lines),len(lines[0]))
-# h,w = len(lines[0]),len(lines)
-# chessboard_states=np.zeros((h,w))
-# count = 0
-# for i,line in enumerate(lines):
-    # for j,point in enumerate(line):
-    #     x,y = point
-    #     w,h = coordinates_dict[point]
-    #     # region = dilated[y:y+h,x:x+w]
-    #     if w<42 or h<42:
-    #         shrink_factor = 3
-    #     else:
-    #         shrink_factor = 7  # 定义收窄的像素值
-    #     x_shrinked =  x + shrink_factor  # 边界不超出原图像
-    #     y_shrinked = y + shrink_factor
-    #     w_shrinked = max(0, w - 2 * shrink_factor)  # 收窄宽度
-    #     h_shrinked = max(0, h - 2 * shrink_factor)  # 收窄高度
 
-    #     # 确保收窄后的边界不会超出图像范围
-    #     region = gray_chessboard[y_shrinked:y_shrinked + h_shrinked, x_shrinked:x_shrinked + w_shrinked]
-    #     total_pixels = region.size  # 总像素数
-    #     white_pixels = np.sum(region == 255)  # 白色像素数
+def show_region(lines,gray_chessboard,shrink_factor=7):
+    for i,line in enumerate(lines):
+        for j,point in enumerate(line):
+            x,y = point
+            w,h = coordinates_dict[point]
+            # region = dilated[y:y+h,x:x+w]
+            if w<42 or h<42:
+                shrink_factor = 3
+            else:
+                shrink_factor = 7  # 定义收窄的像素值
+            x_shrinked =  x + shrink_factor  # 边界不超出原图像
+            y_shrinked = y + shrink_factor
+            w_shrinked = max(0, w - 2 * shrink_factor)  # 收窄宽度
+            h_shrinked = max(0, h - 2 * shrink_factor)  # 收窄高度
 
-    #     # 计算白色像素占比
-    #     white_ratio = white_pixels / total_pixels if total_pixels > 0 else 0
-    #     if white_ratio > 0.05:
-    #         #提取轮廓
-    #         contours, hierarchy = cv2.findContours(region, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+            # 确保收窄后的边界不会超出图像范围
+            region = gray_chessboard[y_shrinked:y_shrinked + h_shrinked, x_shrinked:x_shrinked + w_shrinked]
             
-    #         if len(contours) > 1:
-    #             print(f"区域 ({j}, {i}) 包含多个轮廓")
-    #             #用shrink_factor缩小轮廓
-    #             notion = np.zeros_like(region)
-    #             notion[shrink_factor:h_shrinked-shrink_factor,shrink_factor:w_shrinked-shrink_factor] = region[shrink_factor:h_shrinked-shrink_factor,shrink_factor:w_shrinked-shrink_factor]
-    #             region = notion
-    #             contours, hierarchy = cv2.findContours(region, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    #             print(f"缩小后区域 ({j}, {i}) 包含 {len(contours)} 个轮廓")
-    #         #判断是否为圆形
-    #         if len(contours) == 1:
-    #             #计算轮廓的面积
-    #             area = cv2.contourArea(contours[0])
-    #             #计算周长
-    #             perimeter = cv2.arcLength(contours[0], True)
-    #             #画出轮廓
-    #             contour_image = np.zeros_like(region)
-    #             # 画出轮廓
-    #             cv2.drawContours(contour_image, contours, -1, (255,), 2)  # 填充为白色
-    #             # plt.imshow(contour_image, cmap='gray')
-    #             print(perimeter / (2 * np.pi * np.sqrt(area / np.pi)))
-    #             # plt.title('Contours')
-    #             # plt.show()
-    #             #判断是否为圆形
-    #             if perimeter > 0 and area > 0 and perimeter / (2 * np.pi * np.sqrt(area / np.pi)) < 1.3:
-    #                 # print(f"识别到圆形 ({j}, {i})")
-    #                 # count += 1
-    #                 chessboard_states[j,i]=1
-    #             else:
-    #                 chessboard_states[j,i]=2
-    #             # plt.imshow(region,cmap='gray')
-    #             # plt.show()
-    #         # plt.imshow(region,cmap='gray')
-    #         # plt.show()
-    #         # count += 1
-    #     if j==4:
-    #         print(f"区域 ({j}, {i}) 的白色占比: {white_ratio:.2f}")
-    #         print(w,h)
-    #         plt.imshow(region,cmap='gray')
-    #         plt.show()
-# print(f"共识别 {count} 个圆形")
-# print(chessboard_states)
+            total_pixels = region.size  # 总像素数
+            white_pixels = np.sum(region == 255)  # 白色像素数
+
+            # 计算白色像素占比
+            white_ratio = white_pixels / total_pixels if total_pixels > 0 else 0
+            if white_ratio > 0.1:
+                #提取轮廓
+                contours, hierarchy = cv2.findContours(region, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+                
+                if len(contours) > 1:
+                    print(f"区域 ({j}, {i}) 包含多个轮廓")
+                    logest_contour = max(contours, key=cv2.contourArea)
+                    perimeter = cv2.arcLength(logest_contour, True)
+                    area = cv2.contourArea(logest_contour)
+                    print(perimeter / (2 * np.pi * np.sqrt(area / np.pi)))
+                    #用膨胀
+                    
+                    plt.imshow(region,cmap='gray')
+                    plt.show()
+                    contour_image = np.zeros_like(region)
+                    cv2.drawContours(contour_image, logest_contour, -1, (255,), 2)  # 填充为白色
+                    plt.imshow(contour_image, cmap='gray')
+                    plt.title('Contours')
+                    plt.show()
+                    
+                    
+                    print(f"缩小后区域 ({j}, {i}) 包含 {len(contours)} 个轮廓")
+                #判断是否为圆形
+                if len(contours) == 1:
+                    #计算轮廓的面积
+                    area = cv2.contourArea(contours[0])
+                    #计算周长
+                    perimeter = cv2.arcLength(contours[0], True)
+                    #画出轮廓
+                    contour_image = np.zeros_like(region)
+                    # 画出轮廓
+                    cv2.drawContours(contour_image, contours, -1, (255,), 2)  # 填充为白色
+                    
+                    print(perimeter / (2 * np.pi * np.sqrt(area / np.pi)))
+                    
+                    #判断是否为圆形
+                    if perimeter > 0 and area > 0 and perimeter / (2 * np.pi * np.sqrt(area / np.pi)) < 1.3:
+                        
+                        chessboard_states[j,i]=1
+                    else:
+                        chessboard_states[j,i]=2
+                    
+            if j==3:
+                print(f"区域 ({j}, {i}) 的白色占比: {white_ratio:.2f}")
+                print(w,h)
+                plt.imshow(region,cmap='gray')
+                plt.show()
+
